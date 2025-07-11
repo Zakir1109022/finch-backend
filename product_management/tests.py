ecd83e8ed763b77cf499e9b02069829d3a32ff32
Zakir <zakirhasan1109022@gmail.com>
2025-07-11 12:40:16 +0600

    added test code

--- product_management/tests.py
+++ product_management/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.apps import apps
+from django.test import SimpleTestCase
 
-# Create your tests here.
+from product_management.apps import ProductManagementConfig
+
+
+class ProductManagementConfigTests(SimpleTestCase):
+    def test_app_config_name(self):
+        self.assertEqual(ProductManagementConfig.name, 'product_management')
+
+    def test_app_config_verbose_name(self):
+        self.assertEqual(ProductManagementConfig.verbose_name.strip(), 'Product Management')
+
+    def test_app_config_is_registered(self):
+        app_config = apps.get_app_config('product_management')
+        self.assertIsInstance(app_config, ProductManagementConfig)
